c7fb3ec2a99209e064b020d35b99802a9187c537
vital-xvv <vital.xvv@gmail.com>
2024-02-17 12:20:19 +0200

    Finished initial view. Added Preview

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const formOpenBtn = document.querySelector("#form-open");
+const home = document.querySelector(".home");
+const formContainer = document.querySelector(".form_container");
+const formCloseBtn = document.querySelector(".form_close");
+const signUpBtn = document.querySelector("#signup");
+const loginBtn = document.querySelector("#login");
+const pwShowHide = document.querySelectorAll(".pw_hide");
+
+formOpenBtn.addEventListener("click", () => {
+    home.classList.add("show")
+})
+
+formCloseBtn.addEventListener("click", () => {
+    home.classList.remove("show")
+})
+
+signUpBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.add("active");
+})
+
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.remove("active");
+})
+
+pwShowHide.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let getPwInput = icon.parentElement.querySelector("input");
+        if(getPwInput.type === "password") {
+            getPwInput.type = "text";
+            icon.classList.replace("uil-eye-slash", "uil-eye")
+        }else {
+            getPwInput.type = "password";
+            icon.classList.replace("uil-eye", "uil-eye-slash")
+        }
+    })
+})
